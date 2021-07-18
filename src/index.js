0cb124d494c5f37e27304727237fd89592e549aa
thatgeekyjeff <86982589+thatgeekyjeff@users.noreply.github.com>
2021-07-18 22:50:46 +0100

    Update index.js: Modify default config

--- src/index.js
+++ src/index.js
@@ -4,79 +4,92 @@ import loadBlocks from './blocks';
 export default (editor, opts = {}) => {
   opts.traitsOpts = {
     startVal: {
-      name: 'data-startVal',
       type: 'number',
       placeholder: 'Enter start value...',
-      label: 'Start Value'
+      label: 'Start Value',
+      changeProp: true
     },
     endVal: {
-      name: 'data-endVal',
       type: 'number',
       placeholder: 'Enter end value...',
-      label: 'End Value'
+      label: 'End Value',
+      changeProp: true
     },
     decimalPlaces: {
-      name: 'data-decimalPlaces',
       type: 'number',
       placeholder: 'Enter decimal places...',
-      label: 'Decimal Places'
+      label: 'Decimal Places',
+      changeProp: true
     },
     duration: {
-      name: 'data-duration',
       type: 'number',
       placeholder: 'Enter duration...',
-      label: 'Duration'
+      label: 'Duration',
+      changeProp: true
     },
     useGrouping: {
-      name: 'data-useGrouping',
       type: 'checkbox',
       placeholder: 'Enter use grouping...',
-      label: 'Use Grouping'
+      label: 'Use Grouping',
+      changeProp: true
     },
     useEasing: {
-      name: 'data-useEasing',
       type: 'checkbox',
       placeholder: 'Enter use easing...',
-      label: 'Use Easing'
+      label: 'Use Easing',
+      changeProp: true
     },
     smartEasingThreshold: {
-      name: 'data-smartEasingThreshold',
       type: 'number',
       placeholder: 'Enter smart easing threshold...',
-      label: 'Smart Easing Threshold'
+      label: 'Smart Easing Threshold',
+      changeProp: true
     },
     smartEasingAmount: {
-      name: 'data-smartEasingAmount',
       type: 'number',
       placeholder: 'Enter smart easing amount...',
-      label: 'Smart Easing Amount'
+      label: 'Smart Easing Amount',
+      changeProp: true
     },
     separator: {
-      name: 'data-separator',
       type: 'text',
       placeholder: 'Enter separator...',
-      label: 'Separator'
+      label: 'Separator',
+      changeProp: true
     },
     decimal: {
-      name: 'data-decimal',
       type: 'text',
       placeholder: 'Enter decimal...',
-      label: 'Decimal'
+      label: 'Decimal',
+      changeProp: true
     },
     prefix: {
-      name: 'data-prefix',
       type: 'text',
       placeholder: 'Enter prefix...',
-      label: 'Prefix'
+      label: 'Prefix',
+      changeProp: true
     },
     suffix: {
-      name: 'data-suffix',
       type: 'text',
       placeholder: 'Enter suffix...',
-      label: 'Suffix'
+      label: 'Suffix',
+      changeProp: true
     },
     ...opts.traitsOpts
   }
+
+  opts.traitsOpts.startVal.name = 'data-startVal'
+  opts.traitsOpts.endVal.name = 'data-endVal'
+  opts.traitsOpts.decimalPlaces.name = 'data-decimalPlaces'
+  opts.traitsOpts.duration.name = 'data-duration'
+  opts.traitsOpts.useGrouping.name = 'data-useGrouping'
+  opts.traitsOpts.useEasing.name = 'data-useEasing'
+  opts.traitsOpts.smartEasingThreshold.name = 'data-smartEasingThreshold'
+  opts.traitsOpts.smartEasingAmount.name = 'data-smartEasingAmount'
+  opts.traitsOpts.separator.name = 'data-separator'
+  opts.traitsOpts.decimal.name = 'data-decimal'
+  opts.traitsOpts.prefix.name = 'data-prefix'
+  opts.traitsOpts.suffix.name = 'data-suffix'
 
   const options = {
     blockLabel: 'Count Up',
@@ -89,15 +102,15 @@ export default (editor, opts = {}) => {
     componentTagStyle: {
       padding: '5px'
     },
+    countUpJs: 'https://cdnjs.cloudflare.com/ajax/libs/countup.js/2.0.7/countUp.umd.min.js',
+    countUpEndVal: 100,
     countUpOpts: {
-      startVal: '0'
+      startVal: 0
     },
-    traits: ['startVal', 'decimalPlaces', 'duration', 'useGrouping', 'useEasing', 'smartEasingThreshold', 'smartEasingAmount', 'separator', 'decimal', 'prefix', 'suffix'],
+    traits: ['startVal', 'endVal', 'decimalPlaces', 'duration', 'useGrouping', 'useEasing', 'smartEasingThreshold', 'smartEasingAmount', 'separator', 'decimal', 'prefix', 'suffix'],
     ...opts
   }
 
-  // Add components
   loadComponents(editor, options)
-  // Add blocks
   loadBlocks(editor, options)
 }
